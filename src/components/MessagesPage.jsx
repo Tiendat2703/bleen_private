31309ce28f8048e799366175934d3acd2e18e5dc
Thong <dn7110994@gmail.com>
2025-10-07 11:39:00 +0700

    Add 500-word limit validation for message section with real-time feedback

--- src/components/MessagesPage.jsx
+++ src/components/MessagesPage.jsx
@@ -206,7 +206,9 @@ function MessagesPage() {
           </svg>
         ) : (
           // Play icon (triangle)
-          <img alt="Play" className="block max-w-none size-full" src={stopIcon} />
+          <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M8 6L18 12L8 18V6Z" fill="#17B3C1"/>
+          </svg>
         )}
       </button>
       
