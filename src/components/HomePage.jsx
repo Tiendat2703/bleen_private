52648f007c05c7bfb1ee6efa1232287defd75541
Thong <dn7110994@gmail.com>
2025-10-07 19:32:34 +0700

    Adjust HomePage layout: reduce logo size by 2x, move logo up 100px, increase fingerprint icon to 2.5x size, move fingerprint down 30px

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
-import logoImage from '../images/Home Page (Initial Landing)/Logo chính.png';
+import logoChinh from '../images/Home Page (Initial Landing)/Logo chính.png';
 import vectorIcon from '../images/Home Page (Initial Landing)/Vector.svg';
 
 function HomePage() {
